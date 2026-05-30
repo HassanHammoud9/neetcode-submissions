d1d5a841f7a000f3ff91feaa6bddd63f04c5f0ae
Hassan_hmd <99030374+HassanHammoud9@users.noreply.github.com>
2026-05-31 02:39:12 +0300

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    user_input = int(input("Enter your age: "))
+    return user_input
+
+
+def read_float() -> float:
+    user_input = float(input("Enter the temperature: "))
+    return user_input
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
